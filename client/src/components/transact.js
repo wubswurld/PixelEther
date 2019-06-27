a884f2c3b29ce5306650425408d8796f5d0b6a93
Jack Watters <squawfreeskier@gmail.com>
2019-06-27 02:36:46 -0700

    more

--- client/src/components/transact.js
+++ client/src/components/transact.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Maptok from "./maptokens";
+import Newtoken from "./newtoken";
+import ipfs from "../ipfs";
+import { async } from "q";
 class Trans extends React.Component {
   state = { 
       dataKey: null, 
@@ -11,6 +14,14 @@ class Trans extends React.Component {
       id: '',
       iddata: null,
       tokensarray: [],
+      ipfsHash:null,
+      buffer:'',
+      ethAddress:'',
+      blockNumber:'',
+      transactionHash:'',
+      gasUsed:'',
+      txReceipt: ''  
+
     };
 componentDidMount() {
     const { drizzle, drizzleState } = this.props;
@@ -32,16 +43,35 @@ componentDidMount() {
   componentOnMount() {
         // console.log(this.state.dataKey);
   }
+  captureFile = (event) => {
+    event.stopPropagation()
+    event.preventDefault()
+    const file = event.target.files[0]
+    let reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => this.convertToBuffer(reader)  
+  };
+convertToBuffer = async(reader) => {
+  //file is converted to a buffer for upload to IPFS
+    const buffer = await Buffer.from(reader.result);
+  //set this buffer -using es6 syntax
+    this.setState({buffer});
+};
 handleSubmit = (e) => {
     var mintcoin = this.state.contractstate.methods["mintcoin"];
     var data = mintcoin().send().then((req, res) => {
     })
 }
-newSubmit = (e) => {
+newSubmit = async (e) => {
     const { drizzle, drizzleState } = this.props;
     e.preventDefault();
+    await ipfs.add(this.state.buffer, (err, ipfsHash) => {
+        console.log(err,ipfsHash);
+        //setState by setting ipfsHash to ipfsHash[0].hash 
+        this.setState({ ipfsHash:ipfsHash[0].hash });
+    console.log(this.state.ipfsHash);
     var newcoin = this.state.contractstate.methods["createItem"];
-    var coindata = newcoin(this.state.title, this.state.artist).send(
+    var coindata = newcoin(this.state.title, this.state.artist, this.state.ipfsHash).send(
         {
             gas: 5000000,
             gasPrice: '8000000000'
@@ -50,6 +80,7 @@ newSubmit = (e) => {
         this.setState({data: req})
     })
     this.setState({coindatastate: coindata})
+});
 }
 
 idSubmit = (e) => {
@@ -74,24 +105,20 @@ render() {
     const { drizzleState } = this.props;
     const { Pixelether } = this.props.drizzleState.contracts;
     const {dataKey, contractstate} = this.state;
+    console.log(this.state.buffer);
+    console.log(this.state.ipfsHash);
 return (
     <div>
-    <div>
+    {/* <div>
         <form onSubmit={this.newSubmit}>
                 <input onChange={this.updateValue} className='form-input' id="form-input"value={this.state.title} name='title' type='text' placeholder="Title"/>
                 <input onChange={this.updateValue} className='form-input' name='artist' value={this.state.artist} type='text' placeholder="Artist"/>
                 <br />
                 <button type="submit">submit</button>
         </form>
-    </div>
-    <br />
-    {/* <div>
-        <form onSubmit={this.idSubmit}>
-                <input onChange={this.updateValue} className='form-input' id="form-input"value={this.state.id} name='id' type='text' placeholder="Id"/>
-                <br />
-        <button type="submit">submit</button>
-        </form>
     </div> */}
+    <Newtoken title={this.state.title} artist={this.state.artist} updateValue={this.updateValue} newSubmit={this.newSubmit} captureFile={this.captureFile}></Newtoken>
+    <br />
     <div>
         <h3>My tokens:</h3>
         <Maptok
@@ -99,6 +126,16 @@ return (
           drizzleState={this.state.drizzleState}
           mytoks={this.state.tokensarray}
         ></Maptok>
+        <div className="container">
+          <div class="row justify-content-between">
+    {/* <div class="col-4">
+      One of two columns
+    </div>
+    <div class="col-4">
+      One of two columns
+    </div> */}
+  </div>
+    </div>
     </div>
 </div>
 )
